fce22473a3bdfad2ac4e46de8dabf7c88892bec0
EltonLisboa <elton.lisboa@outlook.com>
2022-07-11 14:40:54 -0300

    Seção 2: DotNet 5

---
+++ Back/src/ProEventos.API/Models/Evento.cs
@@ -0,0 +1,15 @@
+using System.Security.Cryptography.X509Certificates;
+namespace ProEventos.API.Models
+{
+    public class Evento
+    {
+        public int EventoId {get; set;}
+        public string Local {get; set;}
+        public string DataEvento {get; set;}
+        public string Tema {get; set;}
+        public int QtdPessoas {get; set;}
+        public string Lote {get; set;} 
+        public string ImagemURL {get; set;}
+
+    }    
+}
